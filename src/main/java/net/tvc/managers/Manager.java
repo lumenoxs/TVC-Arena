ae1e5a409ac823126d017fa78c091bb4abd809a4
lumenoxs <190932586+lumenoxs@users.noreply.github.com>
2025-09-02 12:04:07 +0100

    i cant remember what i changed so heres a commit message

--- src/main/java/net/tvc/managers/Manager.java
+++ src/main/java/net/tvc/managers/Manager.java
@@ -1,15 +1,8 @@
 package net.tvc.managers;
 
 public class Manager {
-
     protected boolean initialized = false;
 
-    /**
-     * @param registerAtStartUp If true, the manager will be registered at startup
-     * Manager is a class that handles a specific task, use it to organize your code.
-     * Create a new class that extends Manager and override the register() method.
-     * Example: ConfigMgr handles the config.yml file and its values
-     */
     public Manager(boolean ...registerAtStartUp) {
         if (registerAtStartUp.length > 0 && !registerAtStartUp[0])
             return;
@@ -17,23 +10,14 @@ public class Manager {
         this.initialized = true;
     }
 
-    /**
-     * Register the manager tasks
-     */
     public void register() {
         System.out.println("[Manager] Registered: " + name());
     }
 
-    /**
-     * @return The Plugin Instance
-     */
     public final boolean isInitialized() {
         return this.initialized;
     }
 
-    /**
-     * @return The name of the manager class
-     */
     public final String name() {
         return getClass().getSimpleName();
     }
